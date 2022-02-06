a6731c1c03085f83336d1ae2d2b0d39bd9938f66
Jonas Tallhage <jonas@tallhage.se>
2022-02-06 19:40:59 +0100

    Dev version 0.0.000
    Added basic version of MIDI class and replay class for buffer list, skeleton version of audio class.

---
+++ tests/replay/utest_FxlMidi.py
@@ -0,0 +1,31 @@
+import src.fx_lab.replay as replay
+import mido
+import logging
+
+
+replay_logger = replay.logger
+# replay_logger.setLevel(logging.DEBUG)
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.WARNING)
+
+bpm = 120
+fs = 44100
+test_file = './tests/test_data/midi/byzantineBlip_chorus.mid'
+
+mid = mido.MidiFile(test_file, clip=True)
+
+midi = replay.FxlMidi(bpm, fs)
+midi.load_midi_file(test_file)
+
+# Note: The implementation of this test is a bit close to what is being tested,
+#       might not catch concept problems
+seconds_per_tick = 60/(mid.ticks_per_beat * bpm)
+ticks = 0
+for msg in mid.tracks[0]:
+    ticks += msg.time
+    t = ticks * seconds_per_tick
+    sample = int(t * fs)
+    assert msg.type in [x['type'] for x in midi[sample]]
+
+# TODO: Perform more meaningful tests of slice operation
+print(midi[992249:992255])
+print(midi[0:1000000])
---
+++ tests/replay/utest_ReplayBufferList.py
@@ -0,0 +1,54 @@
+from multiprocessing import dummy
+import src.fx_lab.replay as replay
+import mido
+import logging
+from random import randrange
+
+
+replay_logger = replay.logger
+# replay_logger.setLevel(logging.DEBUG)
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.WARNING)
+
+bpm = 120
+fs = 44100
+buffer_size = 10000
+# test_file = './tests/test_data/midi/byzantineBlip_chorus.mid'
+test_file = './tests/test_data/midi/byzantineBlip_verse.mid'
+
+
+midi = replay.FxlMidi(bpm, fs)
+# midi.load_midi_file(test_file)
+
+
+dummy_event = {'type': 'note_on', 'note': 100, 'velocity': 100}
+events_list = []
+for k in range(10):
+    ref_sample = randrange(buffer_size)
+    events_list.append(ref_sample)
+    midi.add_event(k * buffer_size + ref_sample, dummy_event)
+    
+
+
+
+
+
+class RenderClass:
+    def __init__(self):
+        self.buffer_number = 0
+    
+    def render_callback(self, buffer_data):
+        print(self.buffer_number, events_list[self.buffer_number], buffer_data)
+        assert events_list[self.buffer_number] == list(buffer_data['midi'].keys())[0], "Misplaced event"
+        self.buffer_number += 1
+    
+
+render_object = RenderClass()
+
+
+replay = replay.ReplayBufferList(buffer_size=buffer_size, callback=render_object.render_callback, midi=midi)
+# replay = replay.ReplayBufferList(buffer_size=100, midi=midi)
+# replay = replay.ReplayBufferList()
+replay.replay()
+
+
+
